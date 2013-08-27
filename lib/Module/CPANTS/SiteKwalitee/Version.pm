d7fe6209ab2015e3595334ef992af6ef3cce57d7
Kenichi Ishigaki <ishigaki@cpan.org>
2013-08-28 03:54:38 +0900

    added Version stuff (which should be renamed as Provides...)

---
+++ lib/Module/CPANTS/SiteKwalitee/Version.pm
@@ -0,0 +1,161 @@
+package Module::CPANTS::SiteKwalitee::Version;
+use warnings;
+use strict;
+use File::Find;
+use File::Spec::Functions qw(catdir catfile abs2rel splitdir);
+use File::stat;
+use File::Basename;
+use Parse::LocalDistribution;
+use List::Util qw/first/;
+use version;
+
+sub order { 200 }
+
+##################################################################
+# Analyse
+##################################################################
+
+sub analyse {
+    my $class=shift;
+    my $me=shift;
+    my $distdir=$me->distdir;
+
+    no warnings 'once';
+    local $Parse::PMFile::ALLOW_DEV_VERSION = 1;
+    my $parser = Parse::LocalDistribution->new;
+    my $provides = $parser->parse($distdir);
+    for (keys %$provides) {
+        delete $provides->{$_}{parsed};
+        delete $provides->{$_}{filemtime};
+        delete $provides->{$_}{simile};
+        $provides->{$_}{infile} =~ s/^$distdir//;
+    }
+
+    my %versions;
+    $versions{$provides->{$_}{infile}}{$_} = $provides->{$_}{version} for keys %$provides;
+
+    $me->d->{versions} = \%versions;
+
+    return;
+}
+
+
+
+##################################################################
+# Kwalitee Indicators
+##################################################################
+
+sub kwalitee_indicators {
+  return [
+    {
+        name=>'consistent_version',
+        error=>qq{This distribution has .pm files with inconsistent versions.},
+        remedy=>q{Split the distribution, or fix the version numbers to make them consistent (use the highest version number to avoid version downgrade).},
+        code=>sub {
+            my $d=shift;
+
+            my %seen;
+            for my $file (keys %{$d->{versions}}) {
+                for my $package (keys %{$d->{versions}{$file}}) {
+                    my $version = $d->{versions}{$file}{$package};
+                    next if !defined $version;
+                    next if $version eq 'undef';
+                    next if $version eq '0'; # XXX
+                    $seen{$version}++;
+                }
+            }
+            my @versions = keys %seen;
+            return 1 if !@versions; # This is bad, but not inconsistent.
+            return 1 if @versions == 1;
+            $d->{error}{consistent_version} = join ',', sort @versions;
+            return 0;
+        },
+        details=>sub {
+            my $d = shift;
+            return $d->{error}{consistent_version};
+        },
+    },
+    {
+        name=>'package_version_matches_dist_version',
+        error=>qq{None of the package versions in this distribution matches the distribution version.},
+        remedy=>q{Fix the version(s).},
+        code=>sub {
+            my $d=shift;
+
+            my $distv = $d->{version};
+            return 0 unless defined $distv;
+            $distv =~ s/\-TRIAL$//;
+            my $distvv = ($distv =~ /^v/ or $distv =~ s/\./\./g > 1) ? eval { version->new($distv) } : undef;
+            for my $file (keys %{$d->{versions}}) {
+                for my $package (keys %{$d->{versions}{$file}}) {
+                    my $version = $d->{versions}{$file}{$package};
+                    next unless defined $version;
+                    return 1 if $version eq $distv;
+                    return 1 if $distvv and $distvv eq eval { version->new($version) };
+                }
+            }
+            return 0;
+        },
+        details=>sub {
+            my $d = shift;
+            return "None of the package versions in this distribution matches the distribution version.";
+        },
+    },
+];
+}
+
+
+q{Favourite record of the moment:
+  Fat Freddys Drop: Based on a true story};
+
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+Module::CPANTS::SiteKwalitee::Version - Check versions
+
+=head1 SYNOPSIS
+
+Look for packages and their versions by parsing a META file or parsing .pm files
+
+=head1 DESCRIPTION
+
+=head2 Methods
+
+=head3 order
+
+Defines the order in which Kwalitee tests should be run.
+
+Returns C<200>.
+
+=head3 analyse
+
+C<MCK::Version> uses C<Pares::PMFile> to parse .pm files
+
+=head3 kwalitee_indicators
+
+Returns the Kwalitee Indicators datastructure.
+
+=over
+
+=item * has_version_in_each_file
+
+=back
+
+=head1 SEE ALSO
+
+L<Module::CPANTS::Analyse>
+
+=head1 AUTHOR
+
+L<Thomas Klausner|https://metacpan.org/author/domm>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2003–2006, 2009 L<Thomas Klausner|https://metacpan.org/author/domm>
+
+You may use and distribute this module according to the same terms
+that Perl is distributed under.
