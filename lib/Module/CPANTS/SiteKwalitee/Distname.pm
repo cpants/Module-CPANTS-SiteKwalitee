7f9102563611df98240d1ae366930cdc8723664e
Kenichi Ishigaki <ishigaki@cpan.org>
2014-07-31 12:27:55 +0900

    added distname_matches_name_in_meta metric suggested by NEILB++ (RT-97628)

---
+++ lib/Module/CPANTS/SiteKwalitee/Distname.pm
@@ -0,0 +1,100 @@
+package Module::CPANTS::SiteKwalitee::Distname;
+use warnings;
+use strict;
+
+sub order { 15 }
+
+##################################################################
+# Analyse
+##################################################################
+
+sub analyse {
+    my $class=shift;
+    my $me=shift;
+
+    # NOTE: necessary information is gathered while unpacking.
+
+    return;
+}
+
+
+##################################################################
+# Kwalitee Indicators
+##################################################################
+
+sub kwalitee_indicators {
+    return [
+        {
+            name=>'distname_matches_name_in_meta',
+            error=>"The distribution name (eg. 'Foo-Bar' of Foo-Bar-1.42.tar.gz) does not match the 'name' field in META files.",
+            remedy=>q{Use a proper tool to make a distribution. You might also need to fix META files if you keep them in the repository.},
+            code=>sub {
+                my $d = shift;
+                return 1 if !$d->{meta_yml} || !%{$d->{meta_yml}};
+                if (($d->{meta_yml}{name} || '') ne $d->{dist}) {
+                    $d->{error}{distname_matches_name_in_meta} = $d->{meta_yml}{name} || '';
+                    return 0;
+                }
+                return 1;
+            },
+            details=>sub {
+                my $d = shift;
+                return "The distribution name ($d->{dist}) doesn't match the name in META ($d->{meta_yml}{name}).";
+            },
+        },
+    ];
+}
+
+
+q{Favourite record of the moment:
+  Fat Freddys Drop: Based on a true story};
+
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+Module::CPANTS::SiteKwalitee::Distname - Proper Dist name
+
+=head1 SYNOPSIS
+
+Checks if a dist version is well-formed.
+
+=head1 DESCRIPTION
+
+=head2 Methods
+
+=head3 order
+
+Defines the order in which Kwalitee tests should be run.
+
+Returns C<15>.
+
+=head3 analyse
+
+=head3 kwalitee_indicators
+
+Returns the Kwalitee Indicators datastructure.
+
+=over
+
+=item * distname_matches_name_in_meta
+
+=back
+
+=head1 SEE ALSO
+
+L<Module::CPANTS::Analyse>
+
+=head1 AUTHOR
+
+L<Kenichi Ishigaki|https://metacpan.org/author/ishigaki>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2014 Kenichi Ishigaki
+
+You may use and distribute this module according to the same terms
+that Perl is distributed under.
---
+++ xt/kwalitee/distname_matches_name_in_meta.t
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use xt::kwalitee::Test;
+
+xt::kwalitee::Test::run(
+  ['LEEJO/CGI.pm-4.02.tar.gz', 0],
+  ['VLADO/CGI-AuthRegister-1.0.tar.gz', 0],
+  ['BARBIE/CPAN-Testers-WWW-Reports-3.48.tar.gz', 0],
+  ['JESUS/Net--RabbitMQ-0.2.7.tar.gz', 0],
+  ['DRSTEVE/stylehouse-20140427.tar.gz', 0],
+  ['INA/Char/Latin1/Char-Latin1-0.96.tar.gz', 0],
+  ['SADAMS/Mojolicious-Command-generate-upstart-0.02.tar.gz', 0],
+  ['JRUBIN/FT817COMM-0.9.9.tar.gz', 0],
+  ['SHMORIMO/p5-Text-Xslate-Syntax-Any-1.5015.tar.gz', 0],
+  ['LOTTC/perl-OSDial-2.2.9.083.2.tar.gz', 0],
+);
